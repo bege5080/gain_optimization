f5d020f30691234a6ac5796210fa4b9d7e77bc9d
Benjamin Gerard <benjamingerard@MacBook-Pro-8.local>
2017-04-11 21:54:03 -0700

    first commit

--- gain_optimizer.py
+++ gain_optimizer.py
@@ -1,23 +1,21 @@
 '''
-write simple code to optimize the gain by minimizing the atmospheric 
+write simple code to optimize the gain AO gain by minimizing the quadratic sum of atmospheric and noise components of an AO system.
 '''
 
 from matplotlib.widgets import Slider, Button, RadioButtons
 import os
 import sys
-import itertools
-from astropy.io import fits
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
-from scipy.ndimage.filters import gaussian_filter
-from scipy.ndimage.interpolation import rotate
-import scipy.ndimage.interpolation as inter
-from astropy.convolution import convolve,convolve_fft
 import scipy
 
 #all the AO transfer functions
 from transfer_functions import *
+
+fontsize=14
+font = {'family':'Times New Roman', 'size':fontsize}
+mpl.rc('font',**font)
 
 v,D=20.,8. #wind speed and telescope diameter in meters/s and meters, respectively
 #initial values
@@ -26,9 +24,6 @@ pl=-2. #chosen power law to use for T/T PSD
 T_s,tau=1e-3,1e-3
 seeing=1.5 #seeing in arcsec
 m_object=15. #R band magnitude of guide star
-#f=np.logspace(np.log10(0.1),np.log10(500.),500.)
-#freq = lambda T_s: np.logspace(np.log10(0.1),np.log10(1./(2.*T_s)),500.) #only plot up to the Nyquist limit 
-#f=freq(T_s)
 
 gain_ini=np.linspace(0.01,1.,500)
 
@@ -37,7 +32,7 @@ def opt(seeing,ttrms,m_object,v,D,pl,T_s,tau):
 	f=freq(T_s)
 
 	#calculate number of photons coming to the telescope given a R band magnitude
-	f0=1.35e10 #photons/m**2/s, rband, mag 0 star; numbers from JP
+	f0=1.35e10 #photons/m**2/s, rband, mag 0 star
 	flux_object_ini=f0*10.**(-m_object/2.5)
 	tr_atm,qe=0.7,0.8 #assume transmission through the atmosphere, quantum efficiency of CCD
 	flux_object=flux_object_ini*tr_atm*qe
@@ -93,7 +88,7 @@ mwfe=wfe[indgain]
 minwfe=ax.axhline(mwfe,color='purple',lw=2,linestyle='--',alpha=0.5)
 mgain=gain[indgain]
 mingain=ax.axvline(mgain,color='red',lw=2,linestyle='--',alpha=0.5)
-ax.set_title('WFE='+str(round(mwfe,2))+' arcsec rms, g$_{opt}=$'+str(round(mgain,2)))
+ax.set_title('WFE='+str(round(mwfe,3))+' arcsec rms, g$_{opt}=$'+str(round(mgain,3)))
 ax.grid('on')
 ax.set_ylim(0.,1.)
 ax.set_xlim(0.,1.)
@@ -101,21 +96,21 @@ ax.set_xlim(0.,1.)
 
 fig.subplots_adjust(bottom=0.55,top=0.9,left=0.15)
 
-seeing_ax  = fig.add_axes([0.25, 0.4, 0.65, 0.03], axisbg=axis_color)
-ttrms_ax  = fig.add_axes([0.25, 0.35, 0.65, 0.03], axisbg=axis_color)
-m_object_ax  = fig.add_axes([0.25, 0.3, 0.65, 0.03], axisbg=axis_color)
-v_ax  = fig.add_axes([0.25, 0.25, 0.65, 0.03], axisbg=axis_color)
-d_ax  = fig.add_axes([0.25, 0.2, 0.65, 0.03], axisbg=axis_color)
-pl_ax  = fig.add_axes([0.25, 0.15, 0.65, 0.03], axisbg=axis_color)
-ts_ax  = fig.add_axes([0.25, 0.1, 0.65, 0.03], axisbg=axis_color)
-tau_ax  = fig.add_axes([0.25, 0.05, 0.65, 0.03], axisbg=axis_color)
+ttrms_ax  = fig.add_axes([0.25, 0.4, 0.65, 0.03], axisbg=axis_color)
+v_ax  = fig.add_axes([0.25, 0.35, 0.65, 0.03], axisbg=axis_color)
+d_ax  = fig.add_axes([0.25, 0.3, 0.65, 0.03], axisbg=axis_color)
+pl_ax  = fig.add_axes([0.25, 0.25, 0.65, 0.03], axisbg=axis_color)
+m_object_ax  = fig.add_axes([0.25, 0.2, 0.65, 0.03], axisbg=axis_color)
+seeing_ax  = fig.add_axes([0.25, 0.15, 0.65, 0.03], axisbg=axis_color)
+ts_ax  = fig.add_axes([0.25, 0.07, 0.65, 0.03], axisbg=axis_color)
+tau_ax  = fig.add_axes([0.25, 0.03, 0.65, 0.03], axisbg=axis_color)
 
-seeing_slider = Slider(seeing_ax, 'seeing (arcsec)',0.3,2.0, valinit=seeing)
 ttrms_slider = Slider(ttrms_ax, 'tip/tilt rms (arcsec)',0.1,1.5, valinit=ttrms)
-m_object_slider = Slider(m_object_ax, 'target m$_R$',5.,20., valinit=m_object)
 v_slider = Slider(v_ax, 'wind speed (m/s)',2,30, valinit=v)
 d_slider = Slider(d_ax, 'telescope diameter (m)',1.,10., valinit=D)
 pl_slider = Slider(pl_ax, 'tip/tilt PSD power law',-5,-0.5, valinit=pl)
+m_object_slider = Slider(m_object_ax, 'target R mag',5.,20., valinit=m_object)
+seeing_slider = Slider(seeing_ax, 'WFS spot size (arcsec)',0.3,2.0, valinit=seeing)
 ts_slider = Slider(ts_ax, 'T$_s$ (s)',0.1e-3,3e-3, valinit=T_s,valfmt='%.2e')
 tau_slider = Slider(tau_ax, '$\\tau$ (s)',0.1e-3,3e-3, valinit=tau,valfmt='%.2e')
 
@@ -138,7 +133,7 @@ def sliders_on_changed(val):
 	line.set_data(gain,wfe)
 	minwfe.set_ydata(mwfe)
 	mingain.set_xdata(mgain)
-	ax.set_title('WFE='+str(round(mwfe,2))+' arcsec rms, g$_{opt}=$'+str(round(mgain,2)))
+	ax.set_title('WFE='+str(round(mwfe,3))+' arcsec rms, g$_{opt}=$'+str(round(mgain,3)))
 
 
 	fig.canvas.draw_idle()
@@ -153,3 +148,4 @@ ts_slider.on_changed(sliders_on_changed)
 tau_slider.on_changed(sliders_on_changed)
 
 plt.show()
+plt.savefig('gainopt.png')
